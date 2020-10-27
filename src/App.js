0a6078592d70282363f0376942cc07dd3fe9233b
phylyxnbelt <56028969+balabeltmimi@users.noreply.github.com>
2020-10-28 01:50:22 +0700

    Suduku-prototype#0

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import logo from './logo.svg';
+import './App.css';
+
+class Cell extends Component {
+  state = {
+    number: 1
+  }
+  render() {
+    return (
+      <div 
+        onClick={ e=> {
+          this.setState({
+            number: (this.state.number + 1) % 5
+          });
+        }}
+        className="cell"
+      >
+        {this.state.number !== 0 && this.state.number}
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="board">
+          <Cell number={3} />
+          <Cell number={4} />
+          <Cell number={4} />
+          <Cell number={4} />
+          <Cell number={4} />
+          <Cell number={4} />
+          <Cell number={4} />
+          <Cell number={4} />
+          <Cell number={4} />
+          <Cell number={4} />
+          <Cell number={4} />
+          <Cell number={4} />
+          <Cell number={4} />
+          <Cell number={4} />
+          <Cell number={4} />
+          <Cell number={4} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
